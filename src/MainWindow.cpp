9da973988db7092ae19c280bf3d341a95747ff24
ANDreew1987 <andreew.12.87@gmail.com>
2019-04-08 23:17:30 +0700

    .gitignore added

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,31 +1,148 @@
 #include "MainWindow.hpp"
 #include "ui_MainWindow.h"
 
-#include "ImageLib/UI/ImageLibUI.hpp"
+#include <stdexcept>
+#include <vector>
+#include <fstream>
 
-MainWindow::MainWindow(QWidget *parent) :
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+
+#include <QMessageBox>
+#include <QFileDialog>
+
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::runtime_error;
+
+using ByteArray = vector<char>;
+using EVP_MD_CTX_ptr = unique_ptr<EVP_MD_CTX, decltype(&EVP_MD_CTX_free)>;
+
+const int keyLenght = 4096;
+
+static int OpenSSLErrorCB(const char *cs, size_t cslen, void *str)
+{
+    if (str)
+    {
+        reinterpret_cast<std::string*>(str)->append(cs, cslen);
+        return 1;
+    }
+    return 0;
+}
+
+static void HandleErrors() noexcept(false)
+{
+    std::string str;
+    ERR_print_errors_cb(OpenSSLErrorCB, &str);
+    throw runtime_error(str);
+}
+
+void GeneratePublicKey(
+    const string &fileName, 
+    RSA_ptr &rsa) noexcept(false)
+{
+    BIO_ptr bp_public{ BIO_new_file(fileName.c_str(), "wb"), BIO_free };
+    if (PEM_write_bio_RSAPublicKey(bp_public.get(), rsa.get()) != 1)
+    {
+        HandleErrors();
+    }
+}
+
+void GeneratePrivateKey(
+    const string &fileName, 
+    RSA_ptr &rsa) noexcept(false)
+{
+    BIO_ptr bp_private{ BIO_new_file(fileName.c_str(), "wb"), BIO_free };
+    if (PEM_write_bio_RSAPrivateKey(
+        bp_private.get(), rsa.get(), nullptr, nullptr, 0, nullptr, nullptr) != 1)
+    {
+        HandleErrors();
+    }
+}
+
+RSA_ptr CreateRSA(
+    BIGNUM_ptr &bn) noexcept(false)
+{
+    RSA_ptr rsa{ RSA_new(), RSA_free };
+    if (RSA_generate_key_ex(rsa.get(), keyLenght, bn.get(), nullptr) != 1)
+    {
+        HandleErrors();
+    }
+    return rsa;
+}
+
+BIGNUM_ptr CreateBigNum() noexcept(false)
+{
+    BIGNUM_ptr bn{ BN_new(), BN_free };
+    if (BN_set_word(bn.get(), RSA_F4) != 1)
+    {
+        HandleErrors();
+    }
+    return bn;
+}
+
+BIO_ptr LoadKey(
+    const string &fileName) noexcept(false)
+{
+    BIO_ptr input{ BIO_new(BIO_s_file()), BIO_free };
+    if (BIO_read_filename(input.get(), fileName.c_str()) <= 0)
+    {
+        HandleErrors();
+    }
+    return input;
+}
+
+RSA_ptr LoadPrivateKey(
+    const string &fileName) noexcept(false)
+{
+    BIO_ptr input = LoadKey(fileName);
+    RSA *rsa = nullptr;
+    rsa = PEM_read_bio_RSAPrivateKey(input.get(), &rsa, nullptr, nullptr);
+    RSA_ptr result{ rsa, RSA_free };
+    return result;
+}
+
+RSA_ptr LoadPublicKey(
+    const string &fileName) noexcept(false)
+{
+    BIO_ptr input = LoadKey(fileName);
+    RSA *rsa = nullptr;
+    rsa = PEM_read_bio_RSAPublicKey(input.get(), &rsa, nullptr, nullptr);
+    RSA_ptr result{ rsa, RSA_free };
+    return result;
+}
+
+ByteArray ReadFile(
+    const string &fileName)
+{
+    ifstream ifs(fileName, ifstream::binary | ifstream::ate);
+    ifstream::pos_type pos = ifs.tellg();
+    ByteArray result(pos);
+    ifs.seekg(0, ifstream::beg);
+    ifs.read(result.data(), pos);
+    return result;
+}
+
+void WriteFile(
+    const string &fileName,
+    const ByteArray &data)
+{
+    ofstream ofs(fileName, ofstream::binary | ofstream::ate);
+    ofs.write(data.data(), data.size());
+}
+
+MainWindow::MainWindow(
+    QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow),
-	m_FilterProgress(new QProgressBar(this)),
-	m_CurrentWidget(nullptr),
-	m_Filters(GetFilters())
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-	connect(ui->pbApply, SIGNAL(clicked()), this, SLOT(applyFilter()));
-	connect(ui->cbFilters, SIGNAL(currentIndexChanged(int)), this, SLOT(filterSelected(int)));
-
-	m_FilterProgress->setRange(0, 100);
-
-	ui->statusBar->addWidget(m_FilterProgress);
-
-	for (auto f : m_Filters) {
-		f->setParent(this);
-		ui->cbFilters->addItem(f->filterName());
-		connect(f, SIGNAL(progressChanged(int)), m_FilterProgress, SLOT(setValue(int)));
-		connect(f, SIGNAL(filterStarted()), this, SLOT(filterStarted()));
-		connect(f, SIGNAL(filterStopped()), this, SLOT(filterStopped()));
-	}
+    strPrivateKeyFile = ui->leGenPrivateKeyFile->text().toStdString();
+    strPublicKeyFile = ui->leGenPublicKeyFile->text().toStdString();
+    strFile = ui->leFileToSign->text().toStdString();
+    strSign = ui->leSignFile->text().toStdString();
 }
 
 MainWindow::~MainWindow()
@@ -33,31 +150,206 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::applyFilter()
+void MainWindow::GenerateKeyPair(
+    const string &prKeyFileName,
+    const string &pubKeyFileName) noexcept(false)
 {
-	QFilterUI *f = m_Filters[ui->cbFilters->currentIndex()];
-	f->apply();
+    BIGNUM_ptr bn = CreateBigNum();
+    RSA_ptr rsa = CreateRSA(bn);
+    ::GeneratePrivateKey(prKeyFileName, rsa);
+    ::GeneratePublicKey(pubKeyFileName, rsa);
 }
 
-void MainWindow::filterStarted()
+void MainWindow::SignFile(
+    const string &fileName, 
+    const string &prKeyFileName,
+    const string &sigFileName) noexcept(false)
 {
-	ui->pbApply->setEnabled(false);
-	ui->dockWidget->setEnabled(false);
+    RSA_ptr rsa = LoadPrivateKey(prKeyFileName);
+    EVP_PKEY *prKey = EVP_PKEY_new();
+    EVP_PKEY_assign_RSA(prKey, rsa.get());
+    EVP_MD_CTX_ptr rsaSigCtx{ EVP_MD_CTX_create(), EVP_MD_CTX_free };
+    if (EVP_DigestSignInit(rsaSigCtx.get(), nullptr, EVP_sha512(), nullptr, prKey) <= 0)
+    {
+        HandleErrors();
+    }
+    ByteArray inData = ReadFile(fileName);
+    if (EVP_DigestSignUpdate(rsaSigCtx.get(), inData.data(), inData.size()) <= 0)
+    {
+        HandleErrors();
+    }
+    size_t encLen = 0;
+    if (EVP_DigestSignFinal(rsaSigCtx.get(), nullptr, &encLen) <= 0)
+    {
+        HandleErrors();
+    }
+    ByteArray sig(encLen);
+    if (EVP_DigestSignFinal(rsaSigCtx.get(),
+        reinterpret_cast<unsigned char*>(sig.data()), &encLen) <= 0)
+    {
+        HandleErrors();
+    }
+    WriteFile(sigFileName, sig);
 }
 
-void MainWindow::filterStopped()
+void MainWindow::VerifyFile(
+    const string &fileName, 
+    const string &pubKeyFileName, 
+    const string &sigFileName) noexcept(false)
 {
-	ui->pbApply->setEnabled(true);
-	ui->dockWidget->setEnabled(true);
+    RSA_ptr rsa = LoadPublicKey(pubKeyFileName);
+    EVP_PKEY *pubKey = EVP_PKEY_new();
+    EVP_PKEY_assign_RSA(pubKey, rsa.get());
+    EVP_MD_CTX_ptr rsaVerifyCtx{ EVP_MD_CTX_create(), EVP_MD_CTX_free };
+    if (EVP_DigestVerifyInit(rsaVerifyCtx.get(), nullptr, EVP_sha512(), nullptr, pubKey) <= 0)
+    {
+        HandleErrors();
+    }
+    ByteArray inData = ReadFile(fileName);
+    if (EVP_DigestVerifyUpdate(rsaVerifyCtx.get(), inData.data(), inData.size()) <= 0)
+    {
+        HandleErrors();
+    }
+    ByteArray sig = ReadFile(sigFileName);
+    int authStatus = EVP_DigestVerifyFinal(rsaVerifyCtx.get(),
+        reinterpret_cast<unsigned char*>(sig.data()), sig.size());
+    if (authStatus == 1)
+    {
+        return;
+    }
+    else if (authStatus == 0)
+    {
+        throw runtime_error("File " + fileName + " not authentic");
+    }
+    else
+    {
+        HandleErrors();
+    }
 }
 
-void MainWindow::filterSelected(int idx)
+void MainWindow::on_pbGenKeys_clicked()
 {
-	if (m_CurrentWidget) {
-		ui->frame->layout()->removeWidget(m_CurrentWidget);
-	}
-	m_CurrentWidget = m_Filters[idx];
-	if (m_CurrentWidget) {
-		ui->frame->layout()->addWidget(m_CurrentWidget);
-	}
+    try {
+        GenerateKeyPair(strPrivateKeyFile, strPublicKeyFile);
+    }
+    catch (runtime_error &e)
+    {
+        QMessageBox::critical(this, this->windowTitle(), QString(e.what()), QMessageBox::Ok);
+        return;
+    }
+    QMessageBox::information(this, this->windowTitle(), tr("Keys generated"), QMessageBox::Ok);
+}
+
+void MainWindow::on_pbGenPrivateKeyFile_clicked()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,
+        tr("Private key"), QDir::currentPath(), "OpenSSL keys (*.pem)");
+    if (!fileName.isEmpty())
+    {
+        strPrivateKeyFile = fileName.toStdString();
+        ui->leGenPrivateKeyFile->setText(fileName);
+    }
+}
+
+void MainWindow::on_pbGenPublicKeyFile_clicked()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,
+        tr("Public key"), QDir::currentPath(), "OpenSSL keys (*.pem)");
+    if (!fileName.isEmpty())
+    {
+        strPublicKeyFile = fileName.toStdString();
+        ui->leGenPublicKeyFile->setText(fileName);
+    }
+}
+
+void MainWindow::on_pbFileToSign_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("File to sign"), QDir::currentPath(), "All files (*.*)");
+    if (!fileName.isEmpty())
+    {
+        strFile = fileName.toStdString();
+        ui->leFileToSign->setText(fileName);
+    }
+}
+
+void MainWindow::on_pbSignPrivateKey_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Private key"), QDir::currentPath(), "OpenSSL keys (*.pem)");
+    if (!fileName.isEmpty())
+    {
+        strPrivateKeyFile = fileName.toStdString();
+        ui->leSignPrivateKey->setText(fileName);
+    }
+}
+
+void MainWindow::on_pbSignFile_clicked()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,
+        tr("Sign file"), QDir::currentPath(), "Sign files (*.sign)");
+    if (!fileName.isEmpty())
+    {
+        strSign = fileName.toStdString();
+        ui->leSignFile->setText(fileName);
+    }
+}
+
+void MainWindow::on_pbSign_clicked()
+{
+    try {
+        SignFile(strFile, strPrivateKeyFile, strSign);
+    }
+    catch (runtime_error &e)
+    {
+        QMessageBox::critical(this, this->windowTitle(), QString(e.what()), QMessageBox::Ok);
+        return;
+    }
+    QMessageBox::information(this, this->windowTitle(), tr("File signed"), QMessageBox::Ok);
+}
+
+void MainWindow::on_pbFileToVerify_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("File to verify"), QDir::currentPath(), "All files (*.*)");
+    if (!fileName.isEmpty())
+    {
+        strFile = fileName.toStdString();
+        ui->leFileToVerify->setText(fileName);
+    }
+}
+
+void MainWindow::on_pbVerifyPublicKey_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this,
+        tr("Public key"), QDir::currentPath(), "OpenSSL keys (*.pem)");
+    if (!fileName.isEmpty())
+    {
+        strPublicKeyFile = fileName.toStdString();
+        ui->leVerifyPublicKey->setText(fileName);
+    }
+}
+
+void MainWindow::on_pbVerifySignFile_clicked()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,
+        tr("Sign file"), QDir::currentPath(), "Sign files (*.sign)");
+    if (!fileName.isEmpty())
+    {
+        strSign = fileName.toStdString();
+        ui->leSignFile->setText(fileName);
+    }
+}
+
+void MainWindow::on_pbVerify_clicked()
+{
+    try {
+        VerifyFile(strFile, strPublicKeyFile, strSign);
+    }
+    catch (runtime_error &e)
+    {
+        QMessageBox::critical(this, this->windowTitle(), QString(e.what()), QMessageBox::Ok);
+        return;
+    }
+    QMessageBox::information(this, this->windowTitle(), tr("File verifed"), QMessageBox::Ok);
 }
--- src/MainWindow.hpp
+++ src/MainWindow.hpp
@@ -2,12 +2,21 @@
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
-#include <QProgressBar>
 
-#include "ImageLib/UI/FilterUI.hpp"
+#include <string>
+#include <memory>
+
+#include <openssl/rsa.h>
+
+using std::string;
+using std::unique_ptr;
+
+using BIO_ptr = unique_ptr<BIO, decltype(&BIO_free)>;
+using RSA_ptr = unique_ptr<RSA, decltype(&RSA_free)>;
+using BIGNUM_ptr = unique_ptr<BIGNUM, decltype(&BN_free)>;
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow
@@ -20,19 +29,38 @@ public:
 
 public Q_SLOTS:
 
-	void applyFilter();
 
-	void filterStarted();
-	void filterStopped();
-
-	void filterSelected(int);
-
+private slots:
+    void on_pbGenKeys_clicked();
+    void on_pbGenPrivateKeyFile_clicked();
+    void on_pbGenPublicKeyFile_clicked();
+    void on_pbFileToSign_clicked();
+    void on_pbSignPrivateKey_clicked();
+    void on_pbSignFile_clicked();
+    void on_pbSign_clicked();
+    void on_pbFileToVerify_clicked();
+    void on_pbVerifyPublicKey_clicked();
+    void on_pbVerifySignFile_clicked();
+    void on_pbVerify_clicked();
 private:
     Ui::MainWindow *ui;
-	
-	QProgressBar *m_FilterProgress;
-	std::vector<QFilterUI*> m_Filters;
-	QWidget *m_CurrentWidget;
+
+    string strPrivateKeyFile;
+    string strPublicKeyFile;
+    string strFile;
+    string strSign;
+
+    void GenerateKeyPair(
+        const string &prKeyFileName,
+        const string &pubKeyFileName) noexcept(false);
+    void SignFile(
+        const string &fileName, 
+        const string &prKeyFileName,
+        const string &sigFileName) noexcept(false);
+    void VerifyFile(
+        const string &fileName, 
+        const string &pubKeyFileName,
+        const string &sigFileName) noexcept(false);
 };
 
 #endif // MAINWINDOW_HPP
